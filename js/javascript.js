42a7cd389490b21222ea6fe865412733ef2f8af0
mara alexa <maraalexa@MacBook-Pro-van-mara.local>
2025-05-07 09:57:09 +0200

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    document.querySelector('#btn-add').addEventListener('click', function () {
+        getWeather();
+    });
+}
+
+function getWeathr() {
+    const city = document.getElementById('cityInput').value;
+    const apiKey = '2de294d55572f40565e1c6160867cea0';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    // try{
+        
+    // }
+
+    
+}
+
